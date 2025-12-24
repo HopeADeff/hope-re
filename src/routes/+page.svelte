1c53ed0e98ce98ec331faaf3b3226019dc348e23
Coder-Blue <trananhquan1009@gmail.com>
2025-12-25 01:30:07 +0700

    feat: add shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1 class="text-red-500 font-bold text-4xl">Hello Hope:RE</h1>
+<script lang="ts">
+  import { HandIcon } from "@lucide/svelte";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div>
+  <h1 class="text-red-500 font-bold text-4xl">Hello Hope:RE</h1>
+  <Button>
+    <HandIcon />
+    <p>Hello users</p>
+  </Button>
+</div>
