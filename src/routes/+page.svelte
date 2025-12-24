d2b694674a7e7d1863f41559c538185693d98e41
Noah Trần <trananhquan1009@gmail.com>
2025-12-25 01:31:34 +0700

    feat: add tailwindcss/shadcn-svelte (#8)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { HandIcon } from "@lucide/svelte";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div>
+  <h1 class="text-red-500 font-bold text-4xl">Hello Hope:RE</h1>
+  <Button>
+    <HandIcon />
+    <p>Hello users</p>
+  </Button>
+</div>
