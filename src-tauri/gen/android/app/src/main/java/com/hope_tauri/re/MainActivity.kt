6516852d491268251aa83e0c2df8da6fdecbed17
Noah Trần <trananhquan1009@gmail.com>
2025-12-25 00:43:39 +0700

    feat: add Tauri (#7)

---
+++ src-tauri/gen/android/app/src/main/java/com/hope_tauri/re/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hope_tauri.re
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
