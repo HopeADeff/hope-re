26e5f138990b7b31075cf6ad26a9bc31a64baf90
Noah Trần <trananhquan1009@gmail.com>
2025-12-26 23:52:12 +0700

    bug: fix custom window titlebar on other platforms (#12)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
 
+  import { platform } from "@tauri-apps/plugin-os";
   import { WindowTitle } from "$lib/components";
 
   import "../app.css";
 
   const { children }: { children: Snippet } = $props();
+
+  let currentPlatform = $state<string>("");
+  const isWindows = $derived(currentPlatform === "windows");
+
+  if (typeof window !== "undefined") {
+    currentPlatform = platform();
+  }
 </script>
 
-<WindowTitle />
+{#if isWindows}
+  <WindowTitle />
+{/if}
 {@render children()}
